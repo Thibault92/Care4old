48ce28021c5ff1b2353ab0ff1444373faa48fe42
Thibault Berger <thibault.berger92@gmail.com>
2016-01-12 18:45:17 +0100

    MINOR: changing logging page and making ti work with boolean conditions

--- app/src/main/java/com/example/tinyl/care4old/Care4Old.java
+++ app/src/main/java/com/example/tinyl/care4old/Care4Old.java
@@ -1,8 +1,5 @@
 package com.example.tinyl.care4old;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Intent;
@@ -10,14 +7,12 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -57,9 +52,10 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                if (id == R.id.login /*|| id == EditorInfo.IME_NULL*/) {
                     attemptLogin();
                     return true;
                 }
@@ -106,7 +102,18 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+       /* if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }*/
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
@@ -130,9 +137,9 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
         }
     }
 
@@ -146,41 +153,6 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
         return password.length() > 4;
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
@@ -270,18 +242,16 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
             }
 
             // TODO: register the new account here.
-            return true;
+            return doHttpRequest();
         }
 
         @Override
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
-            showProgress(false);
 
             if (success) {
                 //Intent intent = new Intent(Care4Old.this, PatientPage.class);
-                Intent intent = new Intent(Care4Old.this, MainPagePro.class);
-                startActivity(intent);
+                openMainPage();
                 finish();
 
             } else {
@@ -293,8 +263,12 @@ public class Care4Old extends AppCompatActivity implements LoaderManager.LoaderC
         @Override
         protected void onCancelled() {
             mAuthTask = null;
-            showProgress(false);
         }
+    }
+
+    private void openMainPage(){
+        Intent intent = new Intent(Care4Old.this, MainPagePro.class);
+        startActivity(intent);
     }
 
     boolean doHttpRequest() {
--- app/src/main/java/com/example/tinyl/care4old/PatientPage.java
+++ app/src/main/java/com/example/tinyl/care4old/PatientPage.java
@@ -4,13 +4,85 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class PatientPage extends AppCompatActivity {
+
+    TextView birthday;
+    TextView gender;
+    TextView adress;
+    TextView zip;
+    TextView city;
+    TextView phone;
+    TextView mobilePhone;
+    TextView emergency;
+    TextView status;
+    TextView accompaniment;
+    TextView residency;
+    TextView isFinancial;
+    TextView isPedicure;
+    TextView isMaid;
+    TextView isMeal;
+    TextView isAssistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_page);
+
+        chargeTextFields();
+
+    }
+
+    private void chargeTextFields(){
+
+        birthday = (TextView) findViewById(R.id.birthday_bdd);
+        birthday.setText("01/03/1942");
+
+        gender = (TextView) findViewById(R.id.gender_bdd);
+        gender.setText("M");
+
+        adress = (TextView) findViewById(R.id.adress_bdd);
+        adress.setText("42, impasse de la réponse");
+
+        zip = (TextView) findViewById(R.id.zip_bdd);
+        zip.setText("42442");
+
+        city = (TextView) findViewById(R.id.city_bdd);
+        city.setText("Univers");
+
+        phone = (TextView) findViewById(R.id.phone_bdd);
+        phone.setText("+42424242424");
+
+        mobilePhone = (TextView) findViewById(R.id.mobile_phone_bdd);
+        mobilePhone.setText("+6424242424");
+
+        emergency = (TextView) findViewById(R.id.emergency_bdd);
+        emergency.setText("+42emergency");
+
+        status = (TextView) findViewById(R.id.status_bdd);
+        status.setText("Solo");
+
+        accompaniment = (TextView) findViewById(R.id.accompaniment_bdd);
+        accompaniment.setText("PC");
+
+        residency = (TextView) findViewById(R.id.residency_bdd);
+        residency.setText("vaisseau");
+
+        isFinancial = (TextView) findViewById(R.id.is_financial_bdd);
+        isFinancial.setText("Non");
+
+        isPedicure = (TextView) findViewById(R.id.is_pedicure_bdd);
+        isPedicure.setText("Non");
+
+        isMaid = (TextView) findViewById(R.id.is_maid_bdd);
+        isMaid.setText("Non");
+
+        isMeal = (TextView) findViewById(R.id.is_meal_bdd);
+        isMeal.setText("Oui");
+
+        isAssistance = (TextView) findViewById(R.id.is_assistance_bdd);
+        isAssistance.setText("Oui");
 
     }
 
